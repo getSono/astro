4501b23271d784b07875334ab57b9c5db3974ad3
Sono <sono.arbeit@gmail.com>
2025-03-02 18:07:40 +0100

    e

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SiteTitle = 'Starlog';
-export const SiteDescription = 'Welcome to my website!';
+export const SiteTitle = 'Changelog Sono';
+export const SiteDescription = 'ich bin hobbylos :)';
